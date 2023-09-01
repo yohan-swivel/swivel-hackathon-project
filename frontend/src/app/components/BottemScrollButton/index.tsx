60f2180f40f9bdf6f31d24f43ebd1dd0f9834d16
Bhanuka Krish <bhanukakrish@gmail.com>
2023-09-01 11:22:59 +0530

    fix: scrol issue fixed

--- frontend/src/app/components/BottemScrollButton/index.tsx
+++ frontend/src/app/components/BottemScrollButton/index.tsx
@@ -5,13 +5,9 @@ const BottemScrollButton: React.FC<{
   onClick: MouseEventHandler<HTMLDivElement>;
 }> = (props) => {
   return (
-    <div
-      className="relative h-[165px] flex flex-row py-[21.5px] px-[251.8984375px] box-border items-start justify-center"
-      onClick={props.onClick}
-    >
+    <div className="down_arrow" onClick={props.onClick}>
       <img
-        className={`relative w-[33px] h-[57px] overflow-hidden shrink-0 ${styles.vertMove}`}
-        alt=""
+        alt="down_arrow"
         src="/6124b2f58640ceb5e18f1099-scrolldownsmallsvg1.svg"
       />
     </div>
