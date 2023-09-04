086da6548a418e5a978a811eef1e2f1818217203
yohanDevPerera <yohanp@swivelgroup.com.au>
2023-09-04 14:18:49 +0530

    added email functionality

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -7,4 +7,19 @@ module.exports = ({ env }) => ({
       defaultDepth: 3, // Default is 5
     },
   },
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: env("SMTP_HOST", "smtp-mail.outlook.com"),
+        port: env("SMTP_PORT", 587),
+        auth: {
+          user: env("SMTP_USERNAME"),
+          pass: env("SMTP_PASSWORD"),
+        },
+        // ... any custom nodemailer options
+      },
+      settings: {},
+    },
+  },
 });
---
+++ backend/src/api/inquiry/content-types/inquiry/lifecycles.js
@@ -0,0 +1,36 @@
+module.exports = {
+  async afterCreate(event) {
+    const { result } = event;
+    try {
+      const sentStatus = await sendFeedbackEmail(
+        result.email,
+        result.subject,
+        result.message
+      );
+      if (sentStatus) {
+        await sendResponseEmail(result.email);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  },
+};
+
+const sendFeedbackEmail = async (fromEmail, subject, text) => {
+  return await strapi.plugins["email"].services.email.send({
+    to: process.env.EMAIL_TO_FROM,
+    from: fromEmail,
+    subject: subject,
+    html: text,
+  });
+};
+
+const sendResponseEmail = async (toEmail) => {
+  return await strapi.plugins["email"].services.email.send({
+    to: toEmail,
+    from: process.env.EMAIL_TO_FROM,
+    subject: "Hackathon Inquiry",
+    html: `Thank you for reaching out to us with your inquiry about our hackathon event. We appreciate your interest and enthusiasm!
+    We're excited to hear that you're interested in participating in our hackathon. Our team is dedicated to fostering a vibrant and innovative community of hackers, and we're here to assist you in any way we can.`,
+  });
+};
