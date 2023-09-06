e93ff3ced4a40aa7c87b7bf66bba4436adbf129b
yohanDevPerera <yohanp@swivelgroup.com.au>
2023-09-06 23:11:52 +0530

    added page strcut

--- frontend/src/app/privacy-policy/page.tsx
+++ frontend/src/app/privacy-policy/page.tsx
@@ -1,4 +1,17 @@
 export default async function PageRoute() {
-  return <>123</>;
+  return (
+    <>
+      <div className="grid grid-flow-row gap-10">
+        <b className="mainHeader">SWIVELHACK 2023 PRIVACY POLICY </b>
+        <p className="firstPara">
+          This Privacy Policy explains how SwivelTech (“we”, “us”, or “our”)
+          collects, uses, and shares the personal information of our website
+          SwivelHack 2023’s visitors and users. By using our website, you
+          acknowledge that you have read and understood this Privacy Policy. If
+          you do not agree with any part of this Privacy Policy, you should not
+          use our website.
+        </p>
+      </div>
+    </>
+  );
 }
-  
