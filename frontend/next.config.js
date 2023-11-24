00490225be2a6b00f8a18113817e3ea7beaa152c
yohanDevPerera <yohanp@swivelgroup.com.au>
2023-11-24 17:02:07 +0530

    securty poilcy updated

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -16,11 +16,33 @@ const nextConfig = {
         headers: [
           {
             key: "X-Frame-Options",
-            value: "SAMEORIGIN",
+            value: xFrameOptionsValues,
+          },
+          {
+            key: "Permissions-Policy",
+            value: permissionPolicyValues,
+          },
+          {
+            key: "Content-Security-Policy",
+            value: contentSecurityValues,
           },
           {
             key: "X-Content-Type-Options",
             value: "nosniff",
+          },
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: accessControlOriginValue,
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
           },
         ],
       },
